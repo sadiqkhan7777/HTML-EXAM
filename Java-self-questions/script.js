8f910a2e5d86931d8f3dceaf6736f9600e0ee2fa
sadiqkhan7777 <saiqkhansaiqkhan7777@gmail.com>
2025-01-20 22:10:17 +0500

    HTML-EXAM

---
+++ Java-self-questions/script.js
@@ -0,0 +1,36 @@
+// Javascript exercises
+
+// Q1) Write a function create HelloWorld?
+// function myFunc () {
+//     console.log("Hello World");
+// }
+
+// myFunc();
+
+// Q2)What are single-line comments and multi-line comments?
+// Single line comments
+
+// Anwser
+/* Multi Line
+      Comment    */
+
+// Q3)by using math.round generate any number
+
+// let x = Math.round(3.5);
+// console.log(`Random Number ${x}`);
+
+
+// Q4)create an object add your name, age and use an array of objects for our class students
+
+// const myDetails = {
+//     name: "Sadiq Khan",
+//     age: 25,
+//     classStudents: [
+//         { name: "Abdullah", age: 22},
+//         { name: "Mariyam", age: 20},// age num is random 
+//         { name: "Ishmat", age: 25},
+//     ],
+// };
+
+// console.log(myDetails);
+
