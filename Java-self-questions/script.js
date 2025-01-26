75430db55dce0366b6d54aa085906d76211d6a34
sadiq khan <saiqkhan7777@gmail.com>
2025-01-26 21:54:59 +0500

    Update script.js

--- Java-self-questions/script.js
+++ Java-self-questions/script.js
@@ -1,8 +1,6 @@
 // Javascript exercises
 
 // Q1) Write a function create HelloWorld?
-
-// Answer
 // function myFunc () {
 //     console.log("Hello World");
 // }
@@ -12,19 +10,20 @@
 // Q2)What are single-line comments and multi-line comments?
 
 // Anwser
+
 // Single line comments
-/* Multi Line
-      Comment    */
+
+/*      Multi Line
+                            Comment    */
 
 // Q3)by using math.round generate any number
-// Answer
+
 // let x = Math.round(3.5);
-// console.log(`Random Number ${x}`);
+// console.log(`Random Number: ${x}`);
 
 
 // Q4)create an object add your name, age and use an array of objects for our class students
 
-// Answer
 // const myDetails = {
 //     name: "Sadiq Khan",
 //     age: 25,
